70b02dc7b8ad7f27681ae3a6bcc230d0d9dd31f2
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-11 14:38:53 +0700

    add vercel.json

--- src/component/card/CardAttraction.jsx
+++ src/component/card/CardAttraction.jsx
@@ -4,8 +4,8 @@ const CardAttraction = ({ image, place, todo }) => {
     return (
         <div className='flex flex-col border-dashed border-2 border-gray1 rounded-md p-2 font-jost transition duration-400 ease-in-out hover:-translate-y-[5px] hover:shadow-2xl'>
 
-            <div className='relative overflow-hidden rounded-md'>
-                <img src={image} className='w-full h-auto rounded-md overflow-clip transition-all duration-400 ease-in-out hover:scale-110' />
+            <div className='relative overflow-clip rounded-md'>
+                <img src={image} className='w-full h-auto rounded-md  transition-all duration-400 ease-in-out hover:scale-110' />
             </div>
             <div className='p-3'>
                 <h5 className='font-bold text-base text-headingColor'>{place}</h5>
